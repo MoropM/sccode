ad3f0b89a05553ee7749f90a3e2032c9377ba103
MoropM <moroni.perez25@gmail.com>
2025-03-27 16:01:28 -0600

    ADD(package): Se agregó Monaco Editor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './style.css'
 import Split from 'split-grid'
 import { encode, decode } from 'js-base64';
 
+import * as monaco from 'monaco-editor';
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+
 
 const $ = selector => document.querySelector(selector)??null;
 
@@ -10,6 +13,14 @@ const $html = $('#html')
 const $css = $('#css')
 const $iframe = $('iframe')
 
+
+window.MonacoEnvironment = {
+  getWorker(_, label) {
+    if(label==='html') {
+      return new HtmlWorker();
+    }
+  }
+}
 
 Split({
   columnGutters: [{
@@ -22,6 +33,30 @@ Split({
   }]
 })
 
+const {pathname} = window.location;
+const [ rawHtml, rawCss, rawJs ] = pathname.slice(1).split('%7C');
+let htmlVal = decode(rawHtml)??'';
+let cssVal = decode(rawCss)??'';
+let jsVal = decode(rawJs)??'';
+
+const htmlEditor = monaco.editor.create($html, {
+	value: htmlVal,
+	language: 'html',
+  theme: 'vs-dark',
+  minimap: false,
+});
+
+
+// monaco.editor.create($html, {
+// 	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+// 	language: 'javascript'
+// });
+
+/** Creación del HTML que se mostrará en el iFrame
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
 const createHTML = ({html, js, css}) => {
   return `<!doctype html>
   <html lang="es">
@@ -47,11 +82,13 @@ const createHTML = ({html, js, css}) => {
   `
 }
 
+/** Función para inicializar el HTML a visualizar ya renderizado
+ * 
+ */
 const update = () => {
-  const html = $html.value
+  const html = htmlEditor.getValue()
   const js = $js.value
   const css = $css.value
-  
 
   const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`;
   window.history.replaceState(null, null, `/${hashedCode}`);
@@ -61,26 +98,16 @@ const update = () => {
 }
 
 
-const init = () => {
-  const {pathname} = window.location;
-  const [ rawHtml, rawCss, rawJs ] = pathname.slice(1).split('%7C');
-  const html = rawHtml??'';
-  const css = rawCss??'';
-  const js = rawJs??'';
-  if(html!=='' || css!=='' || js!==''){
-    $html.value = decode(html);
-    $css.value = decode(css);
-    $js.value = decode(js);
-
-    update();
-  }
-  else {
-    console.clear();
-  }
-}
-
+htmlEditor.onDidChangeModelContent(update);
+// $html.addEventListener('input', update)
 $js.addEventListener('input', update)
-$html.addEventListener('input', update)
 $css.addEventListener('input', update)
 
-init()
+if(htmlVal.trim()!=='' || cssVal.trim()!=='' || jsVal.trim()!==''){
+  $css.value = decode(cssVal);
+  $js.value = decode(jsVal);
+  update();
+}
+
+
+
